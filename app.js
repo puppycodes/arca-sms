8c6c06cb7151adb01ce5ae912c05f5b62f361ac6
puppycodes <woof@puppy.codes>
2020-06-21 15:06:04 -0700

    update with file for numbers

--- app.js
+++ app.js
@@ -1,18 +1,16 @@
+require('dotenv').config();
 const accountSid = process.env.TWILIO_ACCOUNT_SID;
 const authToken = process.env.TWILIO_AUTH_TOKEN;
 const notifyServiceSid = process.env.NOTIFY_SERVICE_SID;
-
 const client = require('twilio')(accountSid, authToken);
+const fs = require('fs');
+
+let jsonData = fs.readFileSync('./numbers.json');
 
 client.notify.services(notifyServiceSid)
   .notifications.create({
-    toBinding: JSON.stringify([
-      binding_type: 'sms', address: process.env.MY_PHONE_NUMBER,
-      binding_type: 'sms', address: process.env.SECOND_PHONE_NUMBER,
-      // This also works for iOS, Android, and FB Messenger.
-      // You can mix and match binding_type in the toBinding.
-    ]),
-    body: 'You just sent your first message with the Passthrough API!'
+    toBinding: jsonData,
+    body: 'Test Message'
   })
   .then(notification => console.log(notification.sid))
   .catch(error => console.log(error));
